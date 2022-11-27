d08fa4c852e5014d59a2f8abc3a33696283b4478
entrydenied <benjamintak17@gmail.com>
2022-11-27 11:21:22 -0800

    Add operate function

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const add = function(a, b) {
 	return a + b;
-};
+}
 
 const subtract = function(a, b) {
-  return a - b;
-};
+    return a - b;
+}
 
 const multiply = function(a, b) {
 	return a * b;
-};
+}
 
 const divide = function(a, b) {
-  return a / b;
-};
+    return a / b;
+}
+
+const operate = function(a, b) {
+    if (add) {
+        return a + b
+    } else if (subtract) {
+        return a - b;
+    } else if (multiply) {
+        return a * b;
+    } else {
+        return a / b;
+    }
+}
